8d74220c6a2cb826273cfd7f39ae8a15f82b4db8
longhao hu <233764125@qq.com>
2024-08-01 10:46:07 +0800

    Add files

--- train_dist.sh
+++ train_dist.sh
@@ -1,7 +1,7 @@
 GPU_NUM=$1
 CFG=$2
 DATASETS=$3
-OUTPUT_DIR=$4
+OUTPUT_DIR=$4 
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
@@ -11,9 +11,9 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 # (e.g. GroundingDINO pretrain, DINO pretrain, Swin Transformer pretrain.)
 # If you don't want to use any pretrained model, just ignore this parameter.
 
-python -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
-        --output_dir ${OUTPUT_DIR} \
-        -c ${CFG} \
-        --datasets ${DATASETS}  \
-        --pretrain_model_path /path/to/groundingdino_swint_ogc.pth \
-        --options text_encoder_type=/path/to/bert-base-uncased
+python -m torch.distributed.launch  --nproc_per_node=4 main.py \
+        --output_dir /data/baowen20/hlh/output2 \
+        -c /data/baowen20/hlh/Open-GroundingDino/config/cfg_odvg.py \
+        --datasets /data/baowen20/hlh/Open-GroundingDino/config/datasets_mixed_odvg_train.json \
+        --pretrain_model_path /data/baowen20/hlh/Open-GroundingDino/groundingdino_swint_ogc.pth \
+        --options text_encoder_type=/data/baowen20/hlh/Open-GroundingDino/bert-base-uncased 
